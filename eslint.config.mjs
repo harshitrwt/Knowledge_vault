31fe4222ccb9869b38cca1877980f96b1353dd6f
harshitrwt <harshit.rawat1804@gmail.com>
2025-09-05 00:15:44 +0530

    error-3-lints

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,15 @@
-const eslintConfig = [
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
@@ -12,7 +23,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-this-alias": "off",
       "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-explicit-any": "off", // fully off if too noisy
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "warn",
