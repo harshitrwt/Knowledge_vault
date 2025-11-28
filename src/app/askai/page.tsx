816e0c767ae45ebadb817e02eb07ac0a736546b8
harshitrwt <harshit.rawat1804@gmail.com>
2025-11-28 23:25:16 +0530

    fixes UI

--- src/app/askai/page.tsx
+++ src/app/askai/page.tsx
@@ -285,7 +285,7 @@ export default function AskAi() {
                       {copiedIndex === i ? (
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 text-green-400"
+                          className="h-4 w-4 text-gray-500"
                           viewBox="0 0 20 20"
                           fill="currentColor"
                         >
